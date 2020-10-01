c82b4020bdb9f59c1dca99352dc1332f1302c0d7
Alexandra <alex.yakunch@gmail.com>
2020-10-01 14:56:37 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
-}
-
+	if (!array || array.length === 0){
+	  return 0;
+	}else {
+	  return Math.min.apply(null, array)
+	}
+  }
+  
 exports.max = function max (array) {
-  return 0;
+	if (!array || array.length === 0){
+	  return 0;
+	}else {
+	  return Math.max.apply(null, array)
+	}
 }
-
+  
 exports.avg = function avg (array) {
-  return 0;
+	if (!array || array.length === 0){
+	  return 0;
+	}else {
+	  return array.reduce((accum, currentValue) => (accum + currentValue), 0) / array.length;
+	}
 }
